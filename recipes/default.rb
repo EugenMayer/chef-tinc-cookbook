73f067534f684abc8bf2ee34745e7d09fa6f4dc2
em <eugen.mayer@kontextwork.de>
2017-06-01 10:46:07 +0200

    code cleanup

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,6 @@ service 'tinc' do
   action [ :enable, :start ]
 end
 
-
 # we want to override the options passed to `tincd` and include the --logfile option
 template '/etc/default/tinc' do
   source 'tinc.default.erb'
@@ -16,8 +15,8 @@ template '/etc/default/tinc' do
 end
 
 
-node[:tincvpn][:networks].each do |network_name,network|
-  raise "You need to set the host name for the tinc network #{network_name} in [:tincvpn][:networks][#{network_name}][:network][:host][:name]" if network[:host][:name].nil?
+node['tincvpn']['networks'].each do |network_name, network|
+  raise "You need to set the host name for the tinc network #{network_name} in ['tincvpn']['networks'][#{network_name}]['network']['host'][:name]" if network['host']['name'].nil?
 
   directory "/etc/tinc/#{network_name}"
   directory "/etc/tinc/#{network_name}/hosts"
@@ -32,20 +31,20 @@ node[:tincvpn][:networks].each do |network_name,network|
 
   # local host entry in hosts/
   host_addr = node['fqdn']
-  host_addr = node[:tincvpn][:networks][network_name][:host][:address] unless node[:tincvpn][:networks][network_name][:host][:address].nil?
-  template "/etc/tinc/#{network_name}/hosts/#{node[:tincvpn][:networks][network_name][:host][:name]}" do
+  host_addr = node['tincvpn']['networks'][network_name]['host']['address'] unless node['tincvpn']['networks'][network_name]['host']['address'].nil?
+  template "/etc/tinc/#{network_name}/hosts/#{node['tincvpn']['networks'][network_name]['host'][:name]}" do
     source 'host.erb'
     variables(
-        :pub_key => lazy { File.read("/etc/tinc/#{network_name}/rsa_key.pub") },
-        :address => host_addr,
-        :port => node[:tincvpn][:networks][network_name][:network][:port],
-        :subnets => node[:tincvpn][:networks][network_name][:host][:subnets]
+      pub_key: lazy {File.read("/etc/tinc/#{network_name}/rsa_key.pub")},
+      address: host_addr,
+      port: node['tincvpn']['networks'][network_name]['network']['port'],
+      subnets: node['tincvpn']['networks'][network_name]['host']['subnets']
     )
   end
 
   ruby_block "publish-public-key-#{network_name}" do
     block do
-      node.default[:tincvpn][:networks][network_name][:host][:pubkey] = File.read("/etc/tinc/#{network_name}/rsa_key.pub")
+      node.default['tincvpn']['networks'][network_name]['host']['pubkey'] = File.read("/etc/tinc/#{network_name}/rsa_key.pub")
     end
     action :nothing
   end
@@ -55,10 +54,10 @@ node[:tincvpn][:networks].each do |network_name,network|
     template "/etc/tinc/#{network_name}/tinc-#{action}" do
       source "tinc-#{action}.erb"
       mode '0755'
-      variables({
-                    :tunnel_address => network[:network][:tunneladdr],
-                    :tunnel_netmask => network[:network][:tunnelnetmask],
-                })
+      variables(
+        tunnel_address: network['network']['tunneladdr'],
+        tunnel_netmask: network['network']['tunnelnetmask'],
+      )
       notifies :reload, 'service[tinc]', :delayed
     end
   end
@@ -71,23 +70,23 @@ node[:tincvpn][:networks].each do |network_name,network|
   peers = search(:node, "tincvpn_networks_#{network_name}_host_pubkey:*")
 
   peers.each do |peer|
-    host_name = peer[:tincvpn][:networks][network_name][:host][:name]
-    defined_connect_to = node[:tincvpn][:networks][network_name][:host][:connect_to]
+    host_name = peer['tincvpn']['networks'][network_name]['host'][:name]
+    defined_connect_to = node['tincvpn']['networks'][network_name]['host']['connect_to']
 
     # should we connect to the host
     next if defined_connect_to.length && !defined_connect_to.include?(host_name)
 
     host_addr = peer['fqdn']
-    host_addr = peer[:tincvpn][:networks][network_name][:host][:address] unless peer[:tincvpn][:networks][network_name][:host][:address].nil?
-    host_pubkey = peer[:tincvpn][:networks][network_name][:host][:pubkey]
+    host_addr = peer['tincvpn']['networks'][network_name]['host']['address'] unless peer['tincvpn']['networks'][network_name]['host']['address'].nil?
+    host_pubkey = peer['tincvpn']['networks'][network_name]['host']['pubkey']
 
     template "/etc/tinc/#{network_name}/hosts/#{host_name}" do
       source 'host.erb'
       variables(
-          :address => host_addr,
-          :pub_key => host_pubkey,
-          :port => peer[:tincvpn][:networks][network_name][:network][:port],
-          :subnets => peer[:tincvpn][:networks][network_name][:host][:subnets]
+        address: host_addr,
+        pub_key: host_pubkey,
+        port: peer['tincvpn']['networks'][network_name]['network']['port'],
+        subnets: peer['tincvpn']['networks'][network_name]['host']['subnets']
       )
       notifies :reload, 'service[tinc]', :delayed
     end
@@ -101,10 +100,10 @@ node[:tincvpn][:networks].each do |network_name,network|
   template "/etc/tinc/#{network_name}/tinc.conf" do
     source 'tinc.conf.erb'
     variables(
-        :name => network[:host][:name],
-        :port => network[:network][:port],
-        :hosts_ConnectTo => hosts_connect_to,
-        :mode => network[:network][:mode],
+      name: network['host']['name'],
+      port: network['network']['port'],
+      hosts_connect_to: hosts_connect_to,
+      mode: network['network']['mode'],
     )
     notifies :reload, 'service[tinc]', :delayed
   end
@@ -114,7 +113,7 @@ end
 template '/etc/tinc/nets.boot' do
   source 'nets.boot.erb'
   variables(
-      :networks => node[:tincvpn][:networks].keys
+    networks: node['tincvpn']['networks'].keys
   )
   notifies :restart, 'service[tinc]', :delayed
 end
