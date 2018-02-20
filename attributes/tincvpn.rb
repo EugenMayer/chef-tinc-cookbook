2c6314ef60b9e7b27b31b27e64b0d9b047a11194
mrvovanness <someone@linux.local>
2018-02-20 10:28:01 +0500

    Always use symbols as keys in attributes

--- attributes/tincvpn.rb
+++ attributes/tincvpn.rb
@@ -1,21 +1,25 @@
 # choose your network name, right here default - pick what you want
 # you can also define multiple networks, ensure you handle the ports though!
 # mode is either router, switch or hub, see https://www.tinc-vpn.org/documentation/tinc.conf.5
-default[:tincvpn][:networks]['default'][:network][:mode] = 'router'
-default[:tincvpn][:networks]['default'][:network][:port] = 655
+default[:tincvpn][:networks][:default][:network][:mode] = 'router'
+default[:tincvpn][:networks][:default][:network][:port] = 655
 # that is the virtual network the tinc mesh nodes connect to (not your LAN you will join/offer, see subnets)
-default[:tincvpn][:networks]['default'][:network][:tunneladdr] = '172.25.0.1'
-default[:tincvpn][:networks]['default'][:network][:tunnelnetmask] = '255.255.255.0'
+default[:tincvpn][:networks][:default][:network][:tunneladdr] = '172.25.0.1'
+default[:tincvpn][:networks][:default][:network][:tunnelnetmask] = '255.255.255.0'
 # mandatory, you need to set this to a name of the host, like node1 or whatever
-default[:tincvpn][:networks]['default'][:host][:name] = nil
+default[:tincvpn][:networks][:default][:host][:name] = nil
 # which nodes this host should be able to connect to. If you skip, any node in this network will be a connect target
-default[:tincvpn][:networks]['default'][:host][:connect_to] = []
+default[:tincvpn][:networks][:default][:host][:connect_to] = []
 # define the subnets you want to share of your networks, like you LAN or whatever
-default[:tincvpn][:networks]['default'][:host][:subnets] = []
+default[:tincvpn][:networks][:default][:host][:subnets] = []
 # will default to fqdn when not set
-default[:tincvpn][:networks]['default'][:host][:address] = nil
+default[:tincvpn][:networks][:default][:host][:address] = nil
+# enable default network
+default[:tincvpn][:networks][:default][:disabled] = false
+
+
 
 # use zeroconf with automatic ip and dns management
 # see https://www.tinc-vpn.org/examples/zeroconf-ip-and-dns/ for details.
 # When using this option network mode always switch and subnets are always empty array
-default[:tincvpn][:networks]['default'][:host][:avahi_zeroconf_enabled] = false
+default[:tincvpn][:networks][:default][:host][:avahi_zeroconf_enabled] = false
--- resources/attributes_publishing.rb
+++ resources/attributes_publishing.rb
@@ -5,23 +5,22 @@ property :host_pubkey, String, required: true
 property :host_subnets, Array, default: []
 property :network_port, [String, Integer], required: true
 
-
 action :publish do
   ruby_block "publish-attributes-for-#{new_resource.network_name}" do
     block do
-      node.normal['tincvpn']['networks'][new_resource.network_name]['host']['name'] =
+      node.normal[:tincvpn][:networks][new_resource.network_name][:host][:name] =
         new_resource.host_name
 
-      node.normal['tincvpn']['networks'][new_resource.network_name]['host']['address'] =
+      node.normal[:tincvpn][:networks][new_resource.network_name][:host][:address] =
         new_resource.host_address
 
-      node.normal['tincvpn']['networks'][new_resource.network_name]['host']['pubkey'] =
+      node.normal[:tincvpn][:networks][new_resource.network_name][:host][:pubkey] =
         new_resource.host_pubkey
 
-      node.normal['tincvpn']['networks'][new_resource.network_name]['host']['subnets'] =
+      node.normal[:tincvpn][:networks][new_resource.network_name][:host][:subnets] =
         new_resource.host_subnets
 
-      node.normal['tincvpn']['networks'][new_resource.network_name]['network']['port'] =
+      node.normal[:tincvpn][:networks][new_resource.network_name][:network][:port] =
         new_resource.network_port
     end
   end
