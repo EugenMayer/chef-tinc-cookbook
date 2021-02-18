8cada2929871a2db52bf7c94b7e94b39bcd05651
em <eugen.mayer@kontextwork.de>
2021-02-18 17:32:02 +0100

    use stable 2.0.0 release since beta is not allowed

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'eugen.mayer@kontextwork.de'
 license          'Apache 2.0'
 description      'Tinc Virtual Private Network'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '2.0.0-beta1'
+version           '2.0.0'
 chef_version      '>= 15'
 
 %w[ ubuntu debian ].each do |os|
